ade0c868a93c9f19b0c0974e6ddf5d5ae61af36c
raghnyav <53251255+raghnyav@users.noreply.github.com>
2019-07-25 10:35:26 +0530

    Add files via upload

--- fibon.py
+++ fibon.py
@@ -1,13 +1,15 @@
+"""Program for Palidrome"""
+
 n= int(input())
-a = 1
+a = 1     """intial inputs"""
 b = 1
 temp = b
 x = [a,b]
-while a < n: 
+while a < n:   """check until n"""
     temp = a+b
     a = b
     b = temp
     if temp > n:
         break
-    x.append(temp)
+    x.append(temp)    """Adds the number to the list"""
 print("Fibo series : {} ".format(x))
